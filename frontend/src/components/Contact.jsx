8a1301a065632dc7b7ff882f21fd570e92165c44
Kousick-krishna <kousickkrish1970@gmail.com>
2025-11-23 08:12:59 +0530

    frontend changes

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -34,12 +34,14 @@ function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:5000/api/clients", {
+ const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/clients`, {
   method: "POST",
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json"
+  },
   body: JSON.stringify(formData)
 });
+
 
 
       const result = await response.json();
